8d614ad415d7931ab7c70691bfdb8c85081ac6fd
Sojeb Sikder <sojebsikder@gmail.com>
2022-05-30 11:34:20 +0600

    main

--- Assets/Script/DragonScript.cs
+++ Assets/Script/DragonScript.cs
@@ -2,65 +2,75 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class DragonScript : MonoBehaviour {
-	 
-	private Rigidbody2D myRigidBody;
-	private Animator myAnimator;
-	private float jumpForce;
-	public bool isAlive;
+public class DragonScript : MonoBehaviour
+{
 
-    void Start () {
+    private Rigidbody2D myRigidBody;
+    private Animator myAnimator;
+    private float jumpForce;
+    public bool isAlive;
+
+    void Start()
+    {
         isAlive = true;
 
-		myRigidBody = gameObject.GetComponent<Rigidbody2D>();
-		myAnimator = gameObject.GetComponent<Animator> ();
+        myRigidBody = gameObject.GetComponent<Rigidbody2D>();
+        myAnimator = gameObject.GetComponent<Animator>();
 
-		jumpForce = 10f;
-		myRigidBody.gravityScale = 5f;
+        jumpForce = 10f;
+        myRigidBody.gravityScale = 5f;
 
-	}
-		
+    }
 
-	// Update is called once per frame
-	void Update () {
-		if (isAlive) {
-			if (Input.GetMouseButton (0)) {
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAlive)
+        {
+            if (Input.GetMouseButton(0))
+            {
                 // resume when player ready
                 Time.timeScale = 1f;
-                Flap ();
-			} 
-			CheckIfDragonVisibleOnScreen ();
-		} 
-	}
+                Flap();
+            }
+            CheckIfDragonVisibleOnScreen();
+        }
+    }
 
 
-	void Flap(){
-		myRigidBody.velocity = 
-			new Vector2 (0,jumpForce);
-		
-		myAnimator.SetTrigger ("Flap");
+    void Flap()
+    {
+        myRigidBody.velocity =
+            new Vector2(0, jumpForce);
 
-	}
+        myAnimator.SetTrigger("Flap");
+
+    }
 
 
-	void OnCollisionEnter2D(Collision2D target) {
-		if (target.gameObject.tag == "Obstacles") {
-			isAlive = false;
-			Time.timeScale = 0f;
+    void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.tag == "Obstacles")
+        {
+            isAlive = false;
+            Time.timeScale = 0f;
             // game over
             LoadMenu();
 
         }
-	}
+    }
 
-	void CheckIfDragonVisibleOnScreen() {
-		if (Mathf.Abs(gameObject.transform.position.y) > 5.3f) {
-			isAlive = false;
-			Time.timeScale = 0f;
+    void CheckIfDragonVisibleOnScreen()
+    {
+        if (Mathf.Abs(gameObject.transform.position.y) > 5.3f)
+        {
+            isAlive = false;
+            Time.timeScale = 0f;
             // game over
             LoadMenu();
         }
-	}
+    }
 
     public void LoadMenu()
     {
